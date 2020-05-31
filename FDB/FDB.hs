bf43c4e59c9fcdf73056cdc7c0207e95b55fbbc4
Hugo Kapp <hugo.kapp@epfl.ch>
2020-05-31 14:18:01 +0200

    Make FDB.FDB a library entry point to many implementations

--- FDB/FDB.hs
+++ FDB/FDB.hs
@@ -1,126 +1,52 @@
-module FDB.FDB where
+module FDB.FDB
+  (
+    Table,
+    Q,
+    SQ,
 
--- Temporary
-import qualified FDB.TreeDisp
+    readT,
+    filterQ,
+    groupByQ,
+    subqMap,
 
-import Data.Functor.Identity
+    Agg,
+    Fold,
+    Fold1,
 
-newtype Table a = Table a
+    mapQ,
+    fetchRow,
+    fetchForeign,
+    mapAgg,
+    sumAgg,
+    avgAgg,
+    count,
 
-newtype Q a = Q a
+    groupByWithKey,
+    orderBy,
+    eqFilter,
+    (-|><|->),
 
-newtype SQ a = SQ a
+    exists,
+    subqFilter,
+    minAgg,
 
-readT :: Table a -> Q a
-readT = undefined
+    NatJoin(..),
+    equiJoin,
+    (<-|><|-),
 
-filterQ :: (a -> Bool) -> Q a -> Q a
-filterQ = undefined
+    mapToQ,
 
-groupByQ :: (Ord k) => (a -> k) -> Q a -> Q (Q a)
-groupByQ = undefined
+    TableRef,
 
-subqMap :: (a -> SQ b) -> Q a -> Q b
-subqMap = undefined
+    rowRef,
 
-data Agg a b s = Agg (s -> a -> s) s (s -> b)
-type Fold a b = Agg a b b
-type Fold1 a = Agg a a a
+    Row,
+    RowRef,
+    RowId,
 
-mapQ :: (a -> b) -> Q a -> Q b
-mapQ = undefined
+    toQ,
+    emptyQ
+  ) where
 
-fetchRow :: RowRef a -> SQ (Row a)
-fetchRow = undefined
-
-fetchForeign :: TableRef a -> SQ a
-fetchForeign = undefined
-
-mapAgg :: Agg b c s -> (a -> b) -> Q a -> SQ c
-mapAgg = undefined
-
-sumAgg :: (Num a) => Fold1 a
-sumAgg = undefined
-
-avgAgg :: (Fractional a) => Agg a a (a, a)
-avgAgg = undefined
-
-count :: (Integral n) => Q a -> SQ n
-count = undefined
-
-instance Functor SQ where
-  fmap = undefined
-
-instance Applicative SQ where
-  pure = undefined
-  (<*>) = undefined
-
-instance Monad SQ where
-  (>>=) = undefined
-
-instance Functor Q where
-  fmap = undefined
-
-instance Applicative Q where
-  pure = undefined
-  (<*>) = undefined
-
-instance Monad Q where
-  (>>=) = undefined
-
-groupByWithKey :: (Ord k) => (a -> k) -> Q a -> Q (k, Q a)
-groupByWithKey = undefined
-
-orderBy :: (Ord b) => (a -> b) -> Q a -> Q a
-orderBy = undefined
-
-eqFilter :: (Eq b) => (a -> b) -> b -> Q a -> Q a
-eqFilter = undefined
-
-class NatJoin a b where
-  (|><|) :: Q a -> Q b -> Q (a, b)
-
-(-|><|->) :: (NatJoin a b) => Q a -> Q b -> Q b
-(-|><|->) = undefined
-
-exists :: Q a -> SQ Bool
-exists = undefined
-
-subqFilter :: (a -> SQ Bool) -> Q a -> Q a
-subqFilter = undefined
-
-minAgg :: (Ord a) => Fold1 a
-minAgg = undefined
-
--- Can make the type checker loop forever
--- See https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/glasgow_exts.html#extension-UndecidableInstances
--- instance (NatJoin a b) => NatJoin b a where
-  -- a |><| b = b |><| a
-
-equiJoin :: (Eq c) => (a -> c) -> (b -> c) -> Q a -> Q b -> Q (a, b)
-equiJoin = undefined
-
-(<-|><|-) :: (NatJoin a b) => Q a -> Q b -> Q a
-(<-|><|-) = undefined
-
-mapToQ :: (a -> Q b) -> SQ a -> Q b
-mapToQ = undefined
-
-type TableRef a = RowRef a
-
-rowRef :: Row a -> RowRef a
-rowRef = undefined
-
-newtype Row a = Row a
-data RowRef a = RowRef RowId a
-
-type RowId = Integer
-
-instance Eq (RowRef a) where
-  (RowRef lid _) == (RowRef rid _) = (lid == rid)
-
-toQ :: SQ a -> Q a
-toQ = undefined
-
-emptyQ :: Q a
-emptyQ = undefined
+-- The currently used implementation for FDB
+import FDB.Undefined as FDBImpl
---
+++ FDB/Undefined.hs
@@ -0,0 +1,123 @@
+module FDB.Undefined where
+
+import Data.Functor.Identity
+
+newtype Table a = Table a
+
+newtype Q a = Q a
+
+newtype SQ a = SQ a
+
+readT :: Table a -> Q a
+readT = undefined
+
+filterQ :: (a -> Bool) -> Q a -> Q a
+filterQ = undefined
+
+groupByQ :: (Ord k) => (a -> k) -> Q a -> Q (Q a)
+groupByQ = undefined
+
+subqMap :: (a -> SQ b) -> Q a -> Q b
+subqMap = undefined
+
+data Agg a b s = Agg (s -> a -> s) s (s -> b)
+type Fold a b = Agg a b b
+type Fold1 a = Agg a a a
+
+mapQ :: (a -> b) -> Q a -> Q b
+mapQ = undefined
+
+fetchRow :: RowRef a -> SQ (Row a)
+fetchRow = undefined
+
+fetchForeign :: TableRef a -> SQ a
+fetchForeign = undefined
+
+mapAgg :: Agg b c s -> (a -> b) -> Q a -> SQ c
+mapAgg = undefined
+
+sumAgg :: (Num a) => Fold1 a
+sumAgg = undefined
+
+avgAgg :: (Fractional a) => Agg a a (a, a)
+avgAgg = undefined
+
+count :: (Integral n) => Q a -> SQ n
+count = undefined
+
+instance Functor SQ where
+  fmap = undefined
+
+instance Applicative SQ where
+  pure = undefined
+  (<*>) = undefined
+
+instance Monad SQ where
+  (>>=) = undefined
+
+instance Functor Q where
+  fmap = undefined
+
+instance Applicative Q where
+  pure = undefined
+  (<*>) = undefined
+
+instance Monad Q where
+  (>>=) = undefined
+
+groupByWithKey :: (Ord k) => (a -> k) -> Q a -> Q (k, Q a)
+groupByWithKey = undefined
+
+orderBy :: (Ord b) => (a -> b) -> Q a -> Q a
+orderBy = undefined
+
+eqFilter :: (Eq b) => (a -> b) -> b -> Q a -> Q a
+eqFilter = undefined
+
+class NatJoin a b where
+  (|><|) :: Q a -> Q b -> Q (a, b)
+
+(-|><|->) :: (NatJoin a b) => Q a -> Q b -> Q b
+(-|><|->) = undefined
+
+exists :: Q a -> SQ Bool
+exists = undefined
+
+subqFilter :: (a -> SQ Bool) -> Q a -> Q a
+subqFilter = undefined
+
+minAgg :: (Ord a) => Fold1 a
+minAgg = undefined
+
+-- Can make the type checker loop forever
+-- See https://downloads.haskell.org/~ghc/latest/docs/html/users_guide/glasgow_exts.html#extension-UndecidableInstances
+-- instance (NatJoin a b) => NatJoin b a where
+  -- a |><| b = b |><| a
+
+equiJoin :: (Eq c) => (a -> c) -> (b -> c) -> Q a -> Q b -> Q (a, b)
+equiJoin = undefined
+
+(<-|><|-) :: (NatJoin a b) => Q a -> Q b -> Q a
+(<-|><|-) = undefined
+
+mapToQ :: (a -> Q b) -> SQ a -> Q b
+mapToQ = undefined
+
+type TableRef a = RowRef a
+
+rowRef :: Row a -> RowRef a
+rowRef = undefined
+
+newtype Row a = Row a
+data RowRef a = RowRef RowId a
+
+type RowId = Integer
+
+instance Eq (RowRef a) where
+  (RowRef lid _) == (RowRef rid _) = (lid == rid)
+
+toQ :: SQ a -> Q a
+toQ = undefined
+
+emptyQ :: Q a
+emptyQ = undefined
