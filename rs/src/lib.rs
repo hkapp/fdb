fe8486969e094a3d4e2eae34b3a7ea8938355fdf
Hugo Kapp <hugo.kapp@epfl.ch>
2021-04-12 20:57:51 +0200

    Make SQLite calls in the Rust FFI

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,6 +1,28 @@
 #![crate_type = "lib"]
+use rusqlite::{Connection, Result};
 
 #[no_mangle]
 pub extern fn bar(x: f64) -> f64 {
+    match baz() {
+        Ok(_) => println!("SQLite success"),
+        Err(_) => println!("SQLite failure")
+    };
+
     x - 1.0
+}
+
+const DB_FILENAME: &str = "../data/fdb.db";
+
+fn baz() -> Result<()> {
+    let conn = Connection::open(DB_FILENAME)?;
+
+    let mut stmt = conn.prepare("SELECT * FROM foo;")?;
+    let mut rows = stmt.query([])?;
+
+    while let Some(row) = rows.next()? {
+        let x: u32 = row.get(0)?;
+        println!("{}", x);
+    }
+
+    Ok(())
 }
